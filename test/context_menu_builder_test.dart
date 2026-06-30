090fe2bb8d1b18d0c834101c85c2a89c511f2471
momadvisor <adadmgr01@gmail.com>
2026-07-01 04:36:14 +0900

    Add markdown selectable context menu builder

---
+++ test/context_menu_builder_test.dart
@@ -0,0 +1,47 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_markdown_plus/flutter_markdown_plus.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() => defineTests();
+
+void defineTests() {
+  group('contextMenuBuilder', () {
+    testWidgets('uses the default context menu builder when omitted', (WidgetTester tester) async {
+      await tester.pumpWidget(const MaterialApp(home: MarkdownBody(data: 'Selectable text', selectable: true)));
+
+      final SelectableText text = tester.widget<SelectableText>(find.byType(SelectableText));
+
+      expect(text.contextMenuBuilder, isNotNull);
+    });
+
+    testWidgets('can suppress the selectable text context menu', (WidgetTester tester) async {
+      await tester.pumpWidget(
+        const MaterialApp(home: MarkdownBody(data: 'Selectable text', selectable: true, contextMenuBuilder: null)),
+      );
+
+      final SelectableText text = tester.widget<SelectableText>(find.byType(SelectableText));
+
+      expect(text.contextMenuBuilder, isNull);
+    });
+
+    testWidgets('passes a custom context menu builder to selectable text', (WidgetTester tester) async {
+      Widget customContextMenuBuilder(BuildContext context, EditableTextState editableTextState) {
+        return const SizedBox.shrink();
+      }
+
+      await tester.pumpWidget(
+        MaterialApp(
+          home: MarkdownBody(data: 'Selectable text', selectable: true, contextMenuBuilder: customContextMenuBuilder),
+        ),
+      );
+
+      final SelectableText text = tester.widget<SelectableText>(find.byType(SelectableText));
+
+      expect(text.contextMenuBuilder, same(customContextMenuBuilder));
+    });
+  });
+}
